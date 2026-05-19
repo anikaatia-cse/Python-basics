f454030308ea419df97c21faebd5bf1bfb39689f
anikaatia-cse <anikaatia23@gmail.com>
2026-05-19 22:06:00 +0600

    Uploading yesterday's unfinished work!
    I wanted to upload them one by one, but then I thought, why not upload them all together! That's why it took one day of my streak.

---
+++ operators_demo.py
@@ -0,0 +1,31 @@
+# My starting numbers
+x = 20
+y = 5
+
+#1. Plus
+print("Addition:")
+print(x + y)
+
+#2. Minus
+print("Substraction:")
+print(x - y)
+
+#3. Multiplication
+print("Multiplication:")
+print(x * y)
+
+#4. Division
+print("Division:")
+print(x / y)
+
+#5. Modulo
+print("Modulo:")
+print(x % y)
+
+#6. Floor Division
+print("Floor Division:")
+print(x // y)
+
+#7. Exponent
+print("Exponent:")
+print(x ** y)
